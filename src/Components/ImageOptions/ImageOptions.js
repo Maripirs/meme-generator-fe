0875a5795b1daba1fdb3f8e7d4af105b3bb86e92
Maripirs <mariapazmaluenda@gmail.com>
2023-01-12 12:43:57 -0800

    implemented Image Options component

--- src/Components/ImageOptions/ImageOptions.js
+++ src/Components/ImageOptions/ImageOptions.js
@@ -1,6 +1,68 @@
 import "./ImageOptions.css";
+import { useState, useEffect } from "react";
+const ImageOptions = (props) => {
+	const [randomImages, setRandomImages] = useState([]);
+	const [allImages, setAllImages] = useState(null);
 
-const ImageOptions = () => {
-	return <div>ImageOptions</div>;
+	const getImages = async () => {
+		const response = await fetch("https://api.imgflip.com/get_memes");
+		const memes = await response.json();
+		setAllImages(memes);
+	};
+	const randomizeImages = () => {
+		let images = [];
+		for (let i = 0; i < 4; i++) {
+			let randomInd = Math.floor(Math.random() * allImages.data.memes.length);
+			images.push(allImages.data.memes[randomInd].url);
+		}
+		setRandomImages(images);
+	};
+
+	const selectImage = (e) => {
+		console.log("e.target", e.target.closest("div").id);
+		props.setCurrentImage(e.target.closest("div").id);
+	};
+
+	useEffect(() => {
+		getImages();
+	}, []);
+
+	useEffect(() => {
+		if (allImages) {
+			randomizeImages();
+			props.setCurrentImage(randomImages[0]);
+		}
+	}, [allImages]);
+
+	const selectedImages = randomImages.map((image, i) => {
+		return (
+			<div
+				className={
+					"option-container " +
+					(props.currentImage
+						? props.currentImage === image
+							? "active"
+							: ""
+						: "")
+				}
+				key={i}
+				onClick={selectImage}
+				id={image}
+			>
+				<img className="option-image " src={image} />
+			</div>
+		);
+	});
+
+	return (
+		<>
+			<div className="image-selector-container">
+				<div className="images-container">
+					{randomImages.length > 0 ? selectedImages : "Loading..."}
+				</div>
+				<button onClick={randomizeImages}>Genetate New Images</button>
+			</div>
+		</>
+	);
 };
 export default ImageOptions;
--- src/Pages/Creator/Creator.js
+++ src/Pages/Creator/Creator.js
@@ -3,14 +3,49 @@ import { useState, useEffect } from "react";
 import ImageOptions from "../../Components/ImageOptions/ImageOptions";
 
 const Creator = () => {
+	const [textSubmited, setTextSubmitted] = useState(true);
 	const [currentImage, setCurrentImage] = useState(null);
-	const [textSubmited, setTextSubmitted] = useState(false);
-	const [memeText, setMemeText] = useState("");
+	const [memeText, setMemeText] = useState("User Text");
+	const [creator, setCreator] = useState("Annonymous");
 
-	const submitText = () => {};
+	const submitText = (e) => {
+		e.preventDefault();
+		if (memeText.length > 0) {
+			setTextSubmitted(true);
+		}
+	};
 
 	const handleMemeText = (e) => {
+		console.log(e.target.value);
 		setMemeText(e.target.value);
+	};
+	const handleCreatorText = (e) => {
+		setCreator(e.target.value);
+	};
+
+	const postMeme = async (memeData) => {
+		try {
+			await fetch(`URL GOES HERE`, {
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(memeData),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const submitMeme = (e) => {
+		e.preventDefault();
+		if (memeText.length > 0) {
+			let newMeme = {
+				text: memeText,
+				creator: creator,
+			};
+			console.log(newMeme);
+		}
 	};
 
 	const inputScreen = (
@@ -29,13 +64,39 @@ const Creator = () => {
 
 	const memeScreen = (
 		<>
-			<ImageOptions />
-			<div className="meme-result">
-				<div className="user-text">DEMO</div>
+			<ImageOptions
+				currentImage={currentImage}
+				setCurrentImage={setCurrentImage}
+			/>
+			<div
+				className="meme-result"
+				style={{ backgroundImage: `url(${currentImage})` }}
+			>
+				<input
+					type="text"
+					placeholder="Enter Meme Text Here"
+					value={memeText}
+					className="memeText"
+					onChange={handleMemeText}
+				/>
 			</div>
+			<form className="meme-form" onSubmit={submitMeme}>
+				<label>Created by</label>
+				<input
+					type="text"
+					value={creator}
+					className="creatorName"
+					onChange={handleCreatorText}
+				/>
+				<input type="submit" onClick={submitMeme}></input>
+			</form>
 		</>
 	);
 
-	return <div>{textSubmited ? memeScreen : inputScreen}</div>;
+	return (
+		<div className="creator-container">
+			{textSubmited ? memeScreen : inputScreen}
+		</div>
+	);
 };
 export default Creator;
