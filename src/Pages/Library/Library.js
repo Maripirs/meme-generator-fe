65c3daee619685de28b6047752d1aa2f6960e624
davidhlee94 <davidhlee94@gmail.com>
2023-01-12 15:15:41 -0800

    changed some things on library

--- src/Pages/Library/Library.js
+++ src/Pages/Library/Library.js
@@ -10,71 +10,56 @@ const Library = () => {
   const handleShow = () => setShow(true);
 
   const getMeme = async () => {
-    // try {
-    //   const response = await fetch(`oururl.heroku.com/${id}`);
-    //   const allMemes = await response.json();
-    //   setMeme(allMemes);
-    // } catch (error) {
-    //   console.log(error);
-    // }
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "0057af3370msh1b587aa13941ffcp17913cjsne632f12d93d4",
-        "X-RapidAPI-Host": "programming-memes-images.p.rapidapi.com",
-      },
+    try {
+      const response = await fetch(`oururl.heroku.com/${id}`);
+      const allMemes = await response.json();
+      setMeme(allMemes);
+    } catch (error) {
+      console.log(error);
+    }
+
+    useEffect(() => {
+      getMeme();
+    }, []);
+
+    const loaded = () => {
+      return (
+        <div className="cards-container">
+          {meme.map((meme) => {
+            return (
+              <div key={meme.id} className="cards">
+                {/* <img src={meme.image} />
+				<h2>{meme.text}</h2> */}
+                <div className="image-container">
+                  <h3 className="meme-text">"Text here"</h3>
+                  <img
+                    className="meme-image"
+                    src={meme.image}
+                    onClick={() => {
+                      handleShow();
+                    }}
+                  />
+                </div>
+                <Modal className="modal" show={show} onHide={handleClose}>
+                  <h1 className="modal-text">Text Here</h1>
+                  <img src={meme.image} />
+                </Modal>
+              </div>
+            );
+          })}
+        </div>
+      );
     };
 
-    fetch("https://programming-memes-images.p.rapidapi.com/v1/memes", options)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        setMeme(response);
-      })
-      .catch((err) => console.error(err));
-  };
+    const loading = () => {
+      return <div>The memes are loading.</div>;
+    };
 
-  useEffect(() => {
-    getMeme();
-  }, []);
-
-  const loaded = () => {
     return (
-      <div className="cards-container">
-        {meme.map((meme) => {
-          return (
-            <div key={meme.id} className="cards">
-              {/* <img src={meme.image} />
-				<h2>{meme.text}</h2> */}
-              <div className="image-container">
-                <h3 className="meme-text">"Text here"</h3>
-                <img
-                  className="meme-image"
-                  src={meme.image}
-                  onClick={() => {
-                    handleShow();
-                  }}
-                />
-              </div>
-              <Modal className="modal" show={show} onHide={handleClose}>
-                <h1 className="modal-text">Text Here</h1>
-                <img src={meme.image} />
-              </Modal>
-            </div>
-          );
-        })}
+      <div className="library-container">
+        <div>{meme ? loaded() : loading()}</div>
       </div>
     );
   };
-
-  const loading = () => {
-    return <div>The memes are loading.</div>;
-  };
-
-  return (
-    <div className="library-container">
-      <div>{meme ? loaded() : loading()}</div>
-    </div>
-  );
 };
 export default Library;
