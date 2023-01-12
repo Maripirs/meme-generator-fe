fe3dc11c9777ab200270340f16d3aa656ad9a380
Maripirs <mariapazmaluenda@gmail.com>
2023-01-12 09:47:06 -0800

    included input

--- src/Pages/Creator/Creator.js
+++ src/Pages/Creator/Creator.js
@@ -1,14 +1,41 @@
 import "./Creator.css";
 import { useState, useEffect } from "react";
-const Creator = () => {
-	return (
-		<div>
-			<div className="image-options"></div>
+import ImageOptions from "../../Components/ImageOptions/ImageOptions";
 
+const Creator = () => {
+	const [currentImage, setCurrentImage] = useState(null);
+	const [textSubmited, setTextSubmitted] = useState(false);
+	const [memeText, setMemeText] = useState("");
+
+	const submitText = () => {};
+
+	const handleMemeText = (e) => {
+		setMemeText(e.target.value);
+	};
+
+	const inputScreen = (
+		<>
+			<form onSubmit={submitText}>
+				<input
+					type="text"
+					placeholder="Enter Meme Text Here"
+					value={memeText}
+					onChange={handleMemeText}
+				/>
+				<input type="submit" />
+			</form>
+		</>
+	);
+
+	const memeScreen = (
+		<>
+			<ImageOptions />
 			<div className="meme-result">
 				<div className="user-text">DEMO</div>
 			</div>
-		</div>
+		</>
 	);
+
+	return <div>{textSubmited ? memeScreen : inputScreen}</div>;
 };
 export default Creator;
